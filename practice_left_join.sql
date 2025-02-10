6b5bfbace6c1dd9d0539e5eaf2911227fd7b3579
klobudskyi <149142813+klobudskyi@users.noreply.github.com>
2025-02-11 01:26:41 +0200

    practice_left_join.sql

--- practice_left_join.sql
+++ practice_left_join.sql
@@ -4,9 +4,9 @@ SELECT
 	, c.LastName
 	, COUNT(DISTINCT g.GenreId) AS NumGenres
 FROM InvoiceLine il
-LEFT JOIN Track t ON il.TrackId = t.TrackId 
-LEFT JOIN Genre g ON t.GenreId = g.GenreId 
-LEFT JOIN Invoice i ON il.InvoiceId = i.InvoiceId
-LEFT JOIN Customer c ON i.CustomerId = c.CustomerId 
+LEFT JOIN Track t 	ON il.TrackId = t.TrackId 
+LEFT JOIN Genre g 	ON t.GenreId = g.GenreId 
+LEFT JOIN Invoice I 	ON il.InvoiceId = i.InvoiceId
+LEFT JOIN Customer c 	ON i.CustomerId = c.CustomerId 
 GROUP BY 1, 2, 3
 HAVING COUNT(DISTINCT g.GenreId) >= 3
